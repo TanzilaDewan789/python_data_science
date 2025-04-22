e8e800785ef4a57a0f76ea3fbd5f062e7531d9d7
TanzilaDewanSamira <tanzila.dewan789@gmail.com>
2025-04-22 13:23:03 +0600

    second commit

--- Day1.py
+++ Day1.py
@@ -1,2 +1,44 @@
+import sys
 #Print in python
 print('Hello, World!')
+
+#checking the version
+print(sys.version)
+
+#Addition
+a= 30+33
+print(a)
+
+
+#Subtraction
+b= 50-60
+print(b)
+
+
+#multiplication
+c=4*5
+print(c)
+
+#1.Division
+d=5*5
+print(d)
+#1.1.Division operation expression
+d1=25/5
+print(d1)
+#1.2.
+d2=25/6
+print(d2)
+#1.3.
+d3=25//6
+print(d3) #Everything will cut down after the decimal point
+
+
+# Mathematical expression (BODMAS)
+#1.1
+e= 30+3*2/2-3 #3*3=6/2=3-3=0+30
+print(e)
+
+#1.2
+e1= (30+3)*2
+print(e1)
+
