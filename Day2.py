889fea54c9e12c044deae3dfa96bdabddf69fde4
TanzilaDewanSamira <tanzila.dewan789@gmail.com>
2025-04-29 09:50:43 +0600

    adding append,extend

--- Day2.py
+++ Day2.py
@@ -1,3 +1,41 @@
 #Creating on list
-List=["Tanzila",1,2,3,4,5,6.0,7.99,"Samira"] #Positive index starting from 0,1,2..left to right
+List=["Tanzila",1,'time',6.0,7.99,"Rain"] #Positive index starting from 0,1,2..left to right
 print(List)
+# List slicing
+print(List[2:5])
+
+
+#.extend- use for added new element in the list
+List.extend(["Vector","Logarithm"])
+print(List)
+
+List.extend("Tom")#will add but separately
+print(List)
+
+#.append-use for one element in the list as nested list
+List.append(["Tim","Charls"])
+print(List)
+
+# Change the element based on the index
+List[0]="Samira"
+print(List)
+
+# Delete the element based on the index
+del(List[7])#Build in method
+print(List)
+
+# Split the string, default is by space
+A='My name is tanzila'
+print(A.split())#Build in  method
+
+# Copy (copy by reference) the list A
+a=['apple','mango',80]
+print(a)
+b=a
+print(b)
+# Examine the copy by reference
+print('b[0]:', b[0])
+a[0]="papaya"
+print(a)
+print('b[0]:',b[0])
+
