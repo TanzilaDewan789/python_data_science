11c962e082787d60eb64e1332a2038a5a029eb60
TanzilaDewanSamira <tanzila.dewan789@gmail.com>
2025-04-29 08:13:47 +0600

    added equation

--- Day1.py
+++ Day1.py
@@ -9,11 +9,9 @@ print(sys.version)
 a= 30+33
 print(a)
 
-
 #Subtraction
 b= 50-60
 print(b)
-
 
 #multiplication
 c=4*5
@@ -22,16 +20,18 @@ print(c)
 #1.Division
 d=5*5
 print(d)
+
 #1.1.Division operation expression
 d1=25/5
 print(d1)
+
 #1.2.
 d2=25/6
 print(d2)
+
 #1.3.
 d3=25//6
 print(d3) #Everything will cut down after the decimal point
-
 
 # Mathematical expression (BODMAS)
 #1.1
@@ -42,3 +42,8 @@ print(e)
 e1= (30+3)*2
 print(e1)
 
+e2= 30+34+50
+print(e2)
+
+e3=e2/50
+print(e3)
