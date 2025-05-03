bfa8e17bf31b6c7baae6ddf4b72dbe4abde3c7f2
TanzilaDewanSamira <tanzila.dewan789@gmail.com>
2025-05-03 14:01:35 +0600

    tuples

---
+++ Day3.py
@@ -0,0 +1,9 @@
+"""In Python, there are different data types: String, Integer, and Float.
+These data types can all be contained in a tuple as follows:
+string='Disco'
+int = 10
+float= 1.2 """
+
+#Tuples:
+tuples=("disco",10,1.2)
+print(tuples)
