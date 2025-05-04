452b109e01a9a0a2888d1fb22689f39b85793d16
TanzilaDewanSamira <tanzila.dewan789@gmail.com>
2025-05-04 15:24:10 +0600

    tuples

---
+++ Tuples_CheatSheet.py
@@ -0,0 +1,18 @@
+fruits = ("apple", "banana", "apple", "orange","apple")
+#count()
+print(fruits.count('apple'))
+
+#index()
+print(fruits[2])
+
+#sum()
+numbers = (1,2,3,4,10)
+print(sum(numbers))
+
+#Min & Max
+print(max(numbers))
+print(min(numbers))
+
+#length
+print(len(numbers))
+print(len(fruits))
