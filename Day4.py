2c11f68b7954a2394412ff3e47257208185ec7e1
TanzilaDewanSamira <tanzila.dewan789@gmail.com>
2025-05-10 20:33:03 +0600

    last quiz

--- Day4.py
+++ Day4.py
@@ -60,3 +60,11 @@ a) Create a dictionary album_sales_dict where the keys are the album name and th
 b) Use the dictionary to find the total sales of Thriller:
 c) Find the names of the albums from the dictionary using the method keys():
 d) Find the values of the recording sales from the dictionary using the method values:"""
+
+album_sales_dict ={'Back in Black':50,
+                   "The Bodyguard ":50,
+                  "Thriller":65 }
+print(album_sales_dict)
+print(album_sales_dict["Thriller"])
+print(album_sales_dict.keys())
+print(album_sales_dict.values())
