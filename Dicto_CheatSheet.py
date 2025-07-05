2efa35cf9fb087d2307c21c61a9e9d6853681ed4
TanzilaDewanSamira <tanzila.dewan789@gmail.com>
2025-07-05 17:00:20 +0600

    add

--- Dicto_CheatSheet.py
+++ Dicto_CheatSheet.py
@@ -2,6 +2,14 @@
 person = {
     "name": "Tanzila Dewan",
     "age":24,
-    "city":'UK'
+    "city":'NY'
 }
+print(person)
+
+#Accessing Values
+print(person["name"]) 
+
+#Add or modify
+person['country']= 'USA' # A new entry will be created.
+person["city"]='Manchester'  # Update the existing value for the same key
 print(person)
