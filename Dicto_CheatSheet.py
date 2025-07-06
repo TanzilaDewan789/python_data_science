fe39096375b83436bc7258457ea1d60d0bf34674
TanzilaDewanSamira <tanzila.dewan789@gmail.com>
2025-07-06 15:45:48 +0600

    adding dictionary

--- Dicto_CheatSheet.py
+++ Dicto_CheatSheet.py
@@ -11,5 +11,26 @@ print(person["name"])
 
 #Add or modify
 person['country']= 'USA' # A new entry will be created.
-person["city"]='Manchester'  # Update the existing value for the same key
+person["city"]='Chicago'  # Update the existing value for the same key
 print(person)
+#update()
+person.update({"Profession": "Engineer"})
+
+#delete
+del person["country"]
+print(person)
+
+#key existence
+print('name' in person)
+
+#Restore/copy()
+person = person.copy()
+print(person)
+
+#keys()
+print(person.keys())
+print(list(person.keys()))#keys in list form
+
+#values()
+print(person.values())
+print(list(person.values()))#values in list form
