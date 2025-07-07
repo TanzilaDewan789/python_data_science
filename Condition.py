22ed19051979892faa418f8cb109fdf50c55ce3d
TanzilaDewanSamira <tanzila.dewan789@gmail.com>
2025-07-07 08:20:47 +0600

    starting

---
+++ Condition.py
@@ -0,0 +1,46 @@
+"""Comparison Operators
+Comparison operations compare some value or operand and based on a condition, produce a Boolean. When comparing two values you can use these operators:
+
+1.equal: ==
+2.not equal: !=
+3.greater than: >
+4.less than: <
+5.greater than or equal to: >=
+6.less than or equal to: <="""
+
+#1. 
+a=6
+if (a==5):
+    print('True')
+
+else:
+    print('False')    
+
+#2.
+b=4
+if(b!=4):
+    print('False')
+
+else:
+     print('True')
+
+# Use Equality sign to compare the strings
+value = 'AC/DC'
+if(value=="The bodyguard"):
+    print("True")
+else: 
+    print("False")    
+
+# Compare characters
+value1 = 'B'
+if(value1>"A"):
+    print('true')
+else: 
+    print("false") # Ascii:- A=65, B=66
+# Compare characters  by multiplication
+
+Multi = 'BA'
+if(value1>"AB"):
+    print('true')
+else: 
+    print("false") 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-fruits.remove("cherry")
-print(fruits)
+false
