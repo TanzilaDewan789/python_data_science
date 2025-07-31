e83ef7e9327d35c4249672583e53d4f71cfefa5f
TanzilaDewanSamira <tanzila.dewan789@gmail.com>
2025-07-31 22:41:05 +0600

    adding index

--- Loop(practice).py
+++ Loop(practice).py
@@ -35,3 +35,10 @@ i =1
 while i<=10:
     print(f"{n}*{i} = {n*i}")
     i+=1
+
+#print the element of following list by using loop[1,4,9,16,25,36,49,64,81,100]
+num =[1,4,9,16,25,36,49,64,81,100]
+idx =0
+while idx<len(num):
+    print(num[idx])
+    idx+=1
