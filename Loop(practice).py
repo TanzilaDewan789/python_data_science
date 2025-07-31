c170d450eaf5be101b3cde779190f0d9540ccc76
TanzilaDewanSamira <tanzila.dewan789@gmail.com>
2025-07-31 21:41:34 +0600

    adding new loops

---
+++ Loop(practice).py
@@ -0,0 +1,10 @@
+#Loops are use to repeat instructions. There are 2 types of loops in python- 
+#1.While Loop
+#2.For loop
+
+#Print & count the word "Hello"
+i=1
+while i<=5:
+    print("Hello")
+    print(i)
+    i+=1
